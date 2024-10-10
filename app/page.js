18920c39819b91b48cb80d612f4623da0c69e328
AliSherNadeem <alisher786389@gmail.com>
2024-10-10 11:26:58 +0500

    fix imports in page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 // src/app/page.js
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
-import HeroSection from "../components/HeroSection";
 import ProductsSection from "@/components/ProductsSection";
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
